d05c9b583d5a7d6b0172c2d1744078a853c8c767
Sakyra Mance <smance@Sakyras-MacBook-Air.local>
2022-08-05 14:54:30 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Hello Classwork");
+
+function myFunction() {
+    let name = "Marinara"
+    return name + " Sauce"
+}
+
+console.log(myFunction())
+
+function marinarasauce(cookName) {
+    return "Hello " + cookName + "!"
+}
+
+console.log(marinarasauce("Spaghetti"))
+
+
+console.log("Heat a medium-large saucepan over medium heat.");
+
+console.log(
+    "Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+
+console.log(
+    "Add 1/4 cup water, 2 cans of crushed tomates, and salt ,and season with black pepper to taste.");
+
+console.log(
+     "Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+
+console.log(
+    "Stir in 1/4 cup roughly chopped fresh basil, salt , and pepper as needed.");
+
